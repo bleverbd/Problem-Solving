61c7494cc306bcf52ef12ee4c28a8be86ec5046e
Maminur Rahman <65271873+bleverbd@users.noreply.github.com>
2020-08-24 23:09:17 +0600

    Inferior Area

---
+++ URI/1188.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,j,k=5,l=7;
+    double sum=0.0,m[12][12];
+    char ch[2];
+    scanf("%s",&ch);
+    for(i=0;i<12;i++)
+    {
+        for(j=0;j<12;j++)
+        {
+            scanf("%lf",&m[i][j]);
+
+        }
+        }
+        for(i=7;i<12;i++)
+    {
+
+        for(j=k;j<l;j++)
+        {
+                sum=sum+m[i][j];
+            }
+            k--;
+            l++;
+    }
+    if(ch[0]=='S')
+        printf("%0.1lf\n",sum);
+    else if (ch[0]=='M')
+        printf("%0.1lf\n",sum/30.0);
+
+    return 0;
+}
