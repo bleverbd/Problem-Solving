e05dc31f3659bc634323e57c4a473fc4ae67b06e
Maminur Rahman <65271873+bleverbd@users.noreply.github.com>
2020-08-24 22:47:16 +0600

    Create 1011.c

---
+++ URI/1011.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int r;
+    double VOLUME,pi=3.14159;
+    scanf("%d",&r);
+    VOLUME=(4.0/3)*pi*pow(r,3);
+    printf("VOLUME = %0.3lf\n",VOLUME);
+    return 0;
+
+
+}
