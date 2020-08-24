ea6cc81b37e0e992ed4fb541312b6795de8cf2dd
Maminur Rahman <65271873+bleverbd@users.noreply.github.com>
2020-08-24 23:28:22 +0600

    Create Time Conversion.cpp

---
+++ HackerRank/Time Conversion.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    int ar[n],i,sum=0;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&ar[i]);
+        sum=sum+ar[i];
+
+    }
+    printf("%d\n",sum);
+    return 0;
+
+
+}
