2f8fbf4b58a432d457ffafa8ed94f244b3305b34
Maminur Rahman <65271873+bleverbd@users.noreply.github.com>
2020-08-24 22:47:52 +0600

    Create 1010.c

---
+++ URI/1010.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main ()
+{
+    int n,u,n1,u1;
+    float p,p1;
+    scanf("%d%d%f%d%d%f",&n,&u,&p,&n1,&u1,&p1);
+    printf("VALOR A PAGAR: R$ %0.2f\n",(u*p)+(u1*p1));
+    return 0;
+
+}
