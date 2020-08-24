8789c4601a45e66cd79f664a4bffe739dd4f2519
Maminur Rahman <65271873+bleverbd@users.noreply.github.com>
2020-08-24 22:49:11 +0600

    Salary

---
+++ URI/1008.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int n,h;
+    float p,s;
+    scanf("%d %d %f",&n,&h,&p);
+    s=p*h;
+    printf("NUMBER = %d\nSALARY = U$ %0.2f\n",n,s);
+    return 0;
+
+
+}
