549cc84adedaaabf3124368d09d84d4ddbd98961
Maminur Rahman <65271873+bleverbd@users.noreply.github.com>
2020-08-25 23:10:40 +0600

    Logical Sequence

---
+++ URI/1144.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+int main()
+{
+    int n,i,j;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+
+        for(j=1;j<=3;j++)
+        {
+            if(j==1)
+            {
+                printf("%d ",i);
+            }
+            if(j==2){
+                printf("%d ",i*i);
+            }
+            if(j==3){
+                printf("%d",i*i*i);
+            }
+
+}
+        printf("\n");
+        for(j=1;j<=3;j++)
+        {
+            if(j==1)
+            {
+                printf("%d ",i);
+            }
+            if(j==2){
+                printf("%d ",i*i+1);
+            }
+            if(j==3){
+                printf("%d",i*i*i+1);
+            }
+
+}
+        printf("\n");
+
+
+    }
+    return 0;
+}
