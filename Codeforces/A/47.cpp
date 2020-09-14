e5eceb8d591bc533e8bfaefc7771cf22a2e84e3f
Maminur Rahman <65271873+bleverbd@users.noreply.github.com>
2020-09-14 10:09:51 +0600

    Triangular numbers

---
+++ Codeforces/A/47.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,x;
+    cin>>n;
+    x=sqrt(8*n+1);
+    if(x*x==(8*n+1))
+    {
+        cout<<"YES";
+    }
+    else
+    cout<<"NO";
+    return 0;
+
+
+}
